b0d00ff83adc608970a1e0599a80da80da86ca27
baranau_an <andrei_baranau1@epam.com>
2022-02-01 15:00:55 +0300

    Added project to repo.

---
+++ Reflection/ReflectionExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+
+namespace Reflection
+{
+    internal static class ReflectionExtensions
+    {
+        // Helper extension method to simplify syntax to create a delegate
+        public static TDelegate CreateDelegate<TDelegate>(this MethodInfo mi, object target = null)
+            => (TDelegate)(object)mi.CreateDelegate(typeof(TDelegate), target);
+    }
+}
---
+++ Reflection/SomeType.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Reflection
+{
+    // This class is used to demonstrate reflection
+    // It has a field, constructor, method, property, and an event
+    internal sealed class SomeType
+    {
+        private int m_someField;
+        public SomeType(ref int x) { x *= 2; }
+        public override string ToString() { return m_someField.ToString(); }
+        public int SomeProp
+        {
+            get { return m_someField; }
+            set
+            {
+                if (value < 1)
+                    throw new ArgumentOutOfRangeException(nameof(value));
+                m_someField = value;
+            }
+        }
+        public event EventHandler SomeEvent;
+        private void NoCompilerWarnings() { SomeEvent.ToString(); }
+    }
+}
